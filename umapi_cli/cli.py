4d87d7081e935510cdcd348f485d9cfee1ecf199
Andrew Dorton <adorton@adobe.com>
2019-09-17 19:54:27 -0600

    user-create command

--- umapi_cli/cli.py
+++ umapi_cli/cli.py
@@ -68,7 +68,7 @@ def user_read(console_name, output_format, email):
     """Get details for a single user"""
     fmtr = _formatter(output_format, _output_handler())
     auth_config = config.read(console_name)
-    umapi_conn = client.create(auth_config, False)
+    umapi_conn = client.create_conn(auth_config, False)
     user = umapi_client.UserQuery(umapi_conn, email).result()
     if not user:
         click.echo('No user found')
@@ -88,7 +88,7 @@ def user_read_all(console_name, output_format, out_file):
     """Get details for all users belonging to a console"""
     fmtr = _formatter(output_format, _output_handler(out_file))
     auth_config = config.read(console_name)
-    umapi_conn = client.create(auth_config, False)
+    umapi_conn = client.create_conn(auth_config, False)
     query = umapi_client.UsersQuery(umapi_conn)
     for user in query:
         fmtr.record(user)
@@ -106,7 +106,7 @@ def group_read(console_name, output_format, group_name):
     """Get details for a single user group"""
     fmtr = _formatter(output_format, _output_handler())
     auth_config = config.read(console_name)
-    umapi_conn = client.create(auth_config, False)
+    umapi_conn = client.create_conn(auth_config, False)
     query = umapi_client.GroupsQuery(umapi_conn)
     group,  = [g for g in query if g['groupName'].lower() == group_name.lower()]
     fmtr.record(group)
@@ -124,8 +124,33 @@ def group_read_all(console_name, output_format, out_file):
     """Get details for all groups in a console"""
     fmtr = _formatter(output_format, _output_handler(out_file))
     auth_config = config.read(console_name)
-    umapi_conn = client.create(auth_config, False)
+    umapi_conn = client.create_conn(auth_config, False)
     query = umapi_client.GroupsQuery(umapi_conn)
     for group in query:
         fmtr.record(group)
     fmtr.write()
+
+
+@app.command()
+@click.help_option('-h', '--help')
+@click.option('-c', '--console-name', help='Short name to assign to the integration config',
+              default='main', show_default=True)
+@click.option('--type', 'user_type', help="User's identity type", metavar='adobeID|enterpriseID|federatedID',
+              default='federatedID', show_default=True)
+@click.option('--email', help="User's email address", required=True)
+@click.option('--username', help="User's username (set to email if omitted)")
+@click.option('--domain', help="User's directory domain (set to username domain if omitted, if username is an "
+                               "email, this must be set to same domain)")
+@click.option('--groups', help="Comma-delimited list of groups to assign user")
+@click.option('--firstname', help="User's first name")
+@click.option('--lastname', help="User's last name")
+@click.option('--country', help="User's two-letter (ISO-3166-1 alpha2) country code", required=True)
+@click.option('-t', '--test', 'test_mode', help="Run command in test mode", default=False, show_default=False,
+              is_flag=True)
+def user_create(console_name, user_type, email, username, domain, groups, firstname, lastname, country, test_mode):
+    auth_config = config.read(console_name)
+    umapi_conn = client.create_conn(auth_config, test_mode)
+    user = client.user_create_action(user_type, email, username, domain, groups, firstname, lastname, country)
+    umapi_conn.execute_single(user)
+    umapi_conn.execute_queued()
+    click.echo("errors: {}".format(user.execution_errors()))
--- umapi_cli/client.py
+++ umapi_cli/client.py
@@ -1,11 +1,28 @@
 import os
+import re
 import umapi_client
 
 IMS_HOST = os.environ.get('UMAPI_CLI_IMS_HOST') or 'ims-na1.adobelogin.com'
 IMS_JWT = os.environ.get('UMAPI_CLI_IMS_JWT') or '/ims/exchange/jwt/'
 UMAPI_URL = os.environ.get('UMAPI_CLI_URL') or 'https://usermanagement.adobe.io/v2/usermanagement'
+USER_TYPES = {
+    'adobeID': umapi_client.IdentityTypes.adobeID,
+    'enterpriseID': umapi_client.IdentityTypes.enterpriseID,
+    'federatedID': umapi_client.IdentityTypes.federatedID,
+}
+VALID_COUNTRY = re.compile(r'^[a-zA-Z]{2}$')
 
 
-def create(auth_config, test_mode):
+def create_conn(auth_config, test_mode):
     return umapi_client.Connection(auth_config['org_id'], auth_dict=auth_config, ims_host=IMS_HOST,
                                    ims_endpoint_jwt=IMS_JWT, user_management_endpoint=UMAPI_URL, test_mode=test_mode)
+
+
+def user_create_action(user_type, email, username, domain, groups, firstname, lastname, country):
+    assert user_type in USER_TYPES, "'{}' is an invalid user type".format(user_type)
+    assert VALID_COUNTRY.match(country), "'{}' is an invalid country code format".format(country)
+    user = umapi_client.UserAction(USER_TYPES[user_type], email, username, domain)
+    user.create(firstname, lastname, country, email)
+    if groups is not None:
+        user.add_to_groups(groups.split(','))
+    return user
