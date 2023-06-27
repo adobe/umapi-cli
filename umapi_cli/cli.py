adb79b2b84ceabe84133298f07854934f9a129ad
Andrew Dorton <adorton@adobe.com>
2023-06-27 16:31:21 -0600

    don't assume groups list will be supplied

--- umapi_cli/cli.py
+++ umapi_cli/cli.py
@@ -142,7 +142,7 @@ def group_read_all(ctx, output_format, out_file):
 @click.option('--username', help="User's username (set to email if omitted)")
 @click.option('--domain', help="User's directory domain (set to username domain if omitted, if username is an "
                                "email, this must be set to same domain)")
-@click.option('--groups', help="Comma-delimited list of groups to assign user", default='', show_default=False)
+@click.option('--groups', help="Comma-delimited list of groups to assign user", required=False)
 @click.option('--firstname', help="User's first name")
 @click.option('--lastname', help="User's last name")
 @click.option('--country', help="User's two-letter (ISO-3166-1 alpha2) country code", required=True)
@@ -163,8 +163,12 @@ def user_create(ctx, user_type, email, username, domain, groups, firstname, last
     """
     umapi_conn = ctx.obj['conn']
     queue = ActionQueue(umapi_conn)
+    if groups is None:
+        groups = []
+    else:
+        groups = groups.split(',')
     queue.queue_user_create_action(user_type, email, country, firstname, lastname,
-                                   username, domain, groups.split(','))
+                                   username, domain, groups)
     queue.execute()
     click.echo("errors: {}".format(queue.errors()))
 
